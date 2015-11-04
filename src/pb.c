20af7004ad6ab4a976ff310e3883f02ef8f7492d
Haroun Shehata <she15549@spengergasse.at>
2015-11-04 13:33:01 +0100

    Teilaufgabe 8 fast fertig

--- src/pb.c
+++ src/pb.c
@@ -1,13 +1,10 @@
 Some sample Content.
 Modifizierter Content.
 Haroun fügt gerade war hinzu für Teilaufgabe 3
-<<<<<<< HEAD:Christoph.txt
-Teilaufgabe 5 rofl xD
 GitHub is super
 Kartoffelbrei
-=======
 Tata
 Teilaufgabe 5 rofl xD
 GitHub is super
->>>>>>> b198ea285abb5f4559bf3a624fba65ecb135f0f1:src/Christoph.txt
 #Mut4life
+Geben sie uns eine eins
